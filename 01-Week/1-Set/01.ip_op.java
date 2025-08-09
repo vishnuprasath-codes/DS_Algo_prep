f744ee67b7aa8473b3159b6c455e98fe274f3643
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-08-09 11:58:50 +0530

    Create 01.ip_op.java

---
+++ 01-Week/1-Set/01.ip_op.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ip_op {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int x=input.nextInt();
+        int y=input.nextInt();
+        for(int i=0;i<y;i++){
+            System.out.println(x);
+        }
+        input.close();
+    }   
+}
