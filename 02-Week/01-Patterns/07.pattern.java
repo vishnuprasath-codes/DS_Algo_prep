8df019be1addcd2f868ea48b3782cdaf89f22dbb
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-08-12 21:43:22 +0530

    Create 07.pattern.java

---
+++ 02-Week/01-Patterns/07.pattern.java
@@ -0,0 +1,17 @@
+/*
+4321
+321
+21
+1
+*/
+public class pattern{
+    public static void main(String[] args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=n-i+1;j>=1;j--){
+                Syst.out.print(j);em
+            }
+            System.out.println("");
+        }  
+    }
+}
