406774df84f8c8dd2c294353c2b4df125ca3611a
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-08-13 20:50:21 +0530

    Create 13.pattern.java

---
+++ 02-Week/01-Patterns/13.pattern.java
@@ -0,0 +1,23 @@
+/*
+1
+23
+456
+78910   
+*/
+public class pattern{
+    public static void main(String[] args){
+        int n=4 ,count=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count);
+                count+=1;
+            }
+            System.out.println("");
+        }
+
+        
+        
+    }
+}
+
+
