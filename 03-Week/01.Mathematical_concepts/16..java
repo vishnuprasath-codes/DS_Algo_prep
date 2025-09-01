6390df6af62d3a6f5effcd696a1b3616d9a78739
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:23:54 +0530

    Update 16..java

--- 03-Week/01.Mathematical_concepts/16..java
+++ 03-Week/01.Mathematical_concepts/16..java
@@ -1,1 +1,8 @@
-
+//16.COUNT SQRT
+class Solution {
+    static int countSquares(int n) {
+        // The number of integers k such that k^2 < n
+        // is simply floor(sqrt(n-1))
+        return (int) Math.sqrt(n - 1);
+    }
+}
