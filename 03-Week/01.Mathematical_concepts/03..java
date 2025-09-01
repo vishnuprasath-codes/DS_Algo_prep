e07ca5ddc95f9f89a5e370159590295455019dcf
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:15:13 +0530

    Update 03..java

--- 03-Week/01.Mathematical_concepts/03..java
+++ 03-Week/01.Mathematical_concepts/03..java
@@ -1,1 +1,18 @@
+//3.FACTORIAL
+class Solution {
+    static int factorial(int n) {
+        // Initialize factorial result as 1
+        // (since multiplying by 1 doesn't change the value)
+        int fact = 1;
+
+        // Loop from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // Multiply current number 'i' with fact
+            fact = fact * i;
+        }
+
+        // Return the final factorial value
+        return fact;
+    }
+}
 
