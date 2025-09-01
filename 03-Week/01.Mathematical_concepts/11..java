365fa165c4611ab51a00f57ffcd4157ca0bd2cae
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:21:11 +0530

    Update 11..java

--- 03-Week/01.Mathematical_concepts/11..java
+++ 03-Week/01.Mathematical_concepts/11..java
@@ -1,1 +1,16 @@
+//11.SUM OF NATURAL NUMBERS
+class Solution {
+    public static int findSum(int n) {
+        // Initialize sum variable to store the result
+        int sum = 0;
 
+        // Loop from 1 to n (inclusive)
+        // In each iteration, add the current number i to sum
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;  // or sum += i
+        }
+
+        // After loop ends, sum will contain (1 + 2 + 3 + ... + n)
+        return sum;  // Return the final result
+    }
+}
