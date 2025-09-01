949849a3e5daf1f784259bf39f1c1db82f6a28d1
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:21:49 +0530

    Update 12..java

--- 03-Week/01.Mathematical_concepts/12..java
+++ 03-Week/01.Mathematical_concepts/12..java
@@ -1,1 +1,18 @@
+//12.SUM OF CUBE OF NATURAL NUMBERS
+// User function Template for Java
+
+class Solution {
+    int sumOfSeries(int n) {
+        // Initialize variable to store the result
+        int sum = 0;
+        
+        // Loop through numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            sum += i * i * i;  // Add the cube of i (i³) to sum
+        }
+        
+        // After loop ends, sum will contain (1³ + 2³ + 3³ + ... + n³)
+        return sum;  // Return the final sum
+    }
+}
 
