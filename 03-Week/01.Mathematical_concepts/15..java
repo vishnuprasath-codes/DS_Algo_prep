7022fa0f11b559f8ebe2fb839e2e5b1d650bf12d
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:23:25 +0530

    Update 15..java

--- 03-Week/01.Mathematical_concepts/15..java
+++ 03-Week/01.Mathematical_concepts/15..java
@@ -1,1 +1,26 @@
-
+//15.REVERSE A NUMBER
+class Solution {
+    public int reverseDigits(int n) {
+        // Variable to store the reversed number
+        int rev_num = 0;
+        
+        // Variable to store the last digit at each step
+        int ld = 0;
+        
+        // Loop until all digits are processed
+        while (n > 0) {
+            // Extract the last digit
+            ld = n % 10;
+            
+            // Append the digit to rev_num
+            // Multiply rev_num by 10 to shift left, then add the digit
+            rev_num = rev_num * 10 + ld;
+            
+            // Remove the last digit from n
+            n = n / 10;
+        }
+        
+        // Return the reversed number
+        return rev_num;
+    }
+}
