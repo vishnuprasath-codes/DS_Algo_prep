e060c4ee587de1df855d703945ec462a23796a94
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:19:18 +0530

    Update 08..java

--- 03-Week/01.Mathematical_concepts/08..java
+++ 03-Week/01.Mathematical_concepts/08..java
@@ -1,1 +1,17 @@
+//8.NTH NUMBER
+class Solution {
+    // Method to find the Nth term in the series (sum of first n natural numbers)
+    static int findNthTerm(int n) {
+        // Initialize sum as 0 (this will store the cumulative total)
+        int sum = 0;
 
+        // Loop runs from 1 to n (inclusive)
+        for (int i = 1; i <= n; i++) {
+            // Add each number i to the sum
+            sum = sum + i;
+        }
+
+        // After the loop ends, 'sum' contains the result (1 + 2 + 3 + ... + n)
+        return sum;
+    }
+}
