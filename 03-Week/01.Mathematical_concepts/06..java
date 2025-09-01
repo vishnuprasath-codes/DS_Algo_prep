b69e8b27227e2767809c461f854b7d58f23a8088
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:17:41 +0530

    Update 06..java

--- 03-Week/01.Mathematical_concepts/06..java
+++ 03-Week/01.Mathematical_concepts/06..java
@@ -1,1 +1,16 @@
-
+//6.GCD
+class Solution {
+    public static int gcd(int a, int b) {
+        // GCD using Euclidean Algorithm
+        
+        // Loop continues until b becomes 0
+        while (b > 0) {
+            int temp = b;   // Store current b (needed later for swapping)
+            b = a % b;     // Update b as remainder of a divided by b
+            a = temp;      // Update a as old b (move forward in algorithm)
+        }
+        
+        // When b becomes 0, a will hold the GCD
+        return a;   
+    }
+}
