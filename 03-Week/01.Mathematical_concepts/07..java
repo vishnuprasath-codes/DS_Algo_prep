2af68a19330277893711aa01211e7700fe236050
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:18:30 +0530

    Update 07..java

--- 03-Week/01.Mathematical_concepts/07..java
+++ 03-Week/01.Mathematical_concepts/07..java
@@ -1,1 +1,24 @@
+//7.MIDDLE OF THREE
+// User function Template for Java
 
+class Solution {
+    int middle(int a, int b, int c) {
+        // check if 'a' lies between 'b' and 'c'
+        // case 1: a > b and a < c   (b < a < c)
+        // case 2: a > c and a < b   (c < a < b)
+        if ((a > b && a < c) || (a > c && a < b)) {
+            return a;  // 'a' is the middle element
+        }
+        // check if 'b' lies between 'a' and 'c'
+        // case 1: b > a and b < c   (a < b < c)
+        // case 2: b > c and b < a   (c < b < a)
+        else if ((b > a && b < c) || (b > c && b < a)) {
+            return b;  // 'b' is the middle element
+        }
+        // if neither 'a' nor 'b' is in the middle,
+        // then 'c' must be the middle element
+        else {
+            return c;
+        }
+    }
+}
