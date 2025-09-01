e5f5ac3747a34ab49c5c687cadba05abc98b0b52
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:15:52 +0530

    Update 04..java

--- 03-Week/01.Mathematical_concepts/04..java
+++ 03-Week/01.Mathematical_concepts/04..java
@@ -1,1 +1,22 @@
+//4.CHECK PRIME NUMBER
+class Solution {
+    static boolean isPrime(int n) {
+        // Prime numbers must be greater than 1
+        if (n <= 1) {
+            return false;
+        }
 
+        // Loop from 2 to √n
+        // If n has a factor larger than √n,
+        // then it must also have a smaller factor already checked
+        for (int i = 2; i * i <= n; i++) {
+            // If n is divisible by i, then it's not prime
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        // If no divisors found, n is prime
+        return true;
+    }
+}
