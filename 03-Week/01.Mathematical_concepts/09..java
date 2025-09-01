7c4f83b06febc1da187459937171aab3094bc228
vishnuprasath-codes <vishnu1sde@gmail.com>
2025-09-01 22:19:56 +0530

    Update 09..java

--- 03-Week/01.Mathematical_concepts/09..java
+++ 03-Week/01.Mathematical_concepts/09..java
@@ -1,1 +1,18 @@
+//9.SPECIAL SERIES
+// User function Template for Java
+class Solution {
+    static long sumOfTheSeries(long n) {
+        // Initialize variables
+        int sum = 0;        // To store the cumulative sum (1 + 2 + ... + i)
+        int series_sum = 0; // To store the sum of all cumulative sums
 
+        // Loop from 1 to n
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;           // Add current number i to cumulative sum
+            series_sum += sum;       // Add cumulative sum to the final result
+        }
+
+        // Return the final result
+        return series_sum;
+    }
+}
